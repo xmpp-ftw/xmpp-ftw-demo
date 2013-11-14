231f70e4d130e9b8823e37fca734fd897771d949
Lloyd Watkin <lloyd.watkin@surevine.com>
2013-11-14 14:26:39 +0000

    Update route handling

--- routes.js
+++ routes.js
@@ -1,117 +1,34 @@
 var cloneextend = require('cloneextend')
 
 module.exports = function(app, configuration) {
-    app.get('/', function(req, res) {
-        var options = cloneextend.clone(configuration)
-        res.render('index', options)
-    })
+    
+    var routes = {
+        '/': 'index',
+        '/demo': 'demo',
+        '/chat': 'chat',
+        '/manual': 'manual',
+        '/manual/core': 'core',
+        '/manual/multi-user-chat': 'multi-user-chat',
+        '/manual/publish-subscribe': 'publish-subscribe',
+        '/manual/in-band-registration': 'in-band-registration',
+        '/manual/service-discovery': 'service-discovery',
+        '/manual/delayed-delivery': 'delayed-delivery',
+        '/manual/data-forms': 'data-forms',
+        '/manual/result-set-management': 'result-set-management',
+        '/manual/out-of-band-data': 'out-of-band-data',
+        '/manual/avatar': 'avatar',
+        '/manual/jabber-search': 'jabber-search',
+        '/manual/message-archive-management': 'message-archive-management',
+        '/manual/extensions': 'extensions',
+        '/manual/unknown-stanza-handling': 'unknown-stanza-handling',
+        '/manual/jabber-rpc': 'jabber-rpc',
+        '/manual/item-parser': 'item-parser'
+    }
 
-    app.get('/demo', function(req, res) {
-        var options = cloneextend.clone(configuration)
-        res.render('demo', options)
-    })
-
-    app.get('/chat', function(req, res) {
-        var options = cloneextend.clone(configuration)
-        res.render('chat', options)
-    })
-
-
-    app.get('/manual', function(req, res) {
-        var options = cloneextend.clone(configuration)
-        res.render('manual', options)
-    })
-
-    app.get('/manual/core', function(req, res) {
-        var options = cloneextend.clone(configuration)
-        res.render('core', options)
-    })
-
-    app.get('/manual/multi-user-chat', function(req, res) {
-        var options = cloneextend.clone(configuration)
-        res.render('multi-user-chat', options)
-    })
-
-    app.get('/manual/publish-subscribe', function(req, res) {
-        var options = cloneextend.clone(configuration)
-        res.render('publish-subscribe', options)
-    })
-
-    app.get('/manual/in-band-registration', function(req, res) {
-        var options = cloneextend.clone(configuration)
-        res.render('in-band-registration', options)
-    })
-
-    app.get('/manual/service-discovery', function(req, res) {
-        var options = cloneextend.clone(configuration)
-        res.render('service-discovery', options)
-    })
-
-    app.get('/manual/delayed-delivery', function(req, res) {
-        var options = cloneextend.clone(configuration)
-        res.render('delayed-delivery', options)
-    })
-
-    app.get('/data-forms', function(req, res) {
-        res.redirect('/manual/data-forms')
-    })
-
-    app.get('/manual/data-forms', function(req, res) {
-        var options = cloneextend.clone(configuration)
-        res.render('data-forms', options)
-    })
-
-    app.get('/manual/result-set-management', function(req, res) {
-        var options = cloneextend.clone(configuration)
-        res.render('result-set-management', options)
-    })
-
-    app.get('/manual/out-of-band-data', function(req, res) {
-        var options = cloneextend.clone(configuration)
-        res.render('out-of-band-data', options)
-    })
-
-    app.get('/manual/avatar', function(req, res) {
-        var options = cloneextend.clone(configuration)
-        res.render('avatar', options)
-    })
-
-    app.get('/manual/jabber-search', function(req, res) {
-        var options = cloneextend.clone(configuration)
-        res.render('jabber-search', options)
-    })
-
-    app.get('/manual/message-archive-management', function(req, res) {
-        var options = cloneextend.clone(configuration)
-        res.render('message-archive-management', options)
-    })
-
-    app.get('/extensions', function(req, res) {
-        res.redirect('/manual/extensions')
-    })
-
-    app.get('/manual/extensions', function(req, res) {
-        var options = cloneextend.clone(configuration)
-        res.render('extensions', options)
-    })
-
-    app.get('/manual/unknown-stanza-handling', function(req, res) {
-        var options = cloneextend.clone(configuration)
-        res.render('unknown-stanza-handling', options)
-    })
-
-    app.get('/manual/jabber-rpc', function(req, res) {
-        var options = cloneextend.clone(configuration)
-        res.render('jabber-rpc', options)
-    })
-
-    app.get('/publishing', function(req, res) {
-        res.redirect('/manual/item-parser')
-    })
-
-    app.get('/manual/item-parser', function(req, res) {
-        var options = cloneextend.clone(configuration)
-        res.render('item-parser', options)
+    Object.keys(routes).forEach(function(route) {
+        app.get(route, function(req, res) {
+            res.render(routes[route], configuration)
+        })
     })
 
     app.get('/*', function(req, res) {
